64a8e3566badff92f1cf2352d9104764474ebabe
Chakir FIKRI <chakir.fikri@gmail.com>
2024-07-18 11:10:46 +0200

    Modification du code de l'application

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import src.mytools as mt
 import streamlit as st
 import cv2
 import mlflow
-import boto3
 import os
 
-# Définir l'URI de suivi MLflow
-mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI","http://ec2-52-209-5-80.eu-west-1.compute.amazonaws.com:5000"))
-
 # Chargement du modèle depuis MLflow
-model_uri = "s3://mlflow-cfikri/937544924322822054/f154eae389294193aed012085329d82b/artifacts/InceptionV3"
-model = mlflow.tensorflow.load_model(model_uri)
+@st.cache_resource
+def load_model():
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://ec2-52-209-5-80.eu-west-1.compute.amazonaws.com:5000"))
+    model_uri = "s3://mlflow-cfikri/937544924322822054/f154eae389294193aed012085329d82b/artifacts/InceptionV3"
+    return mlflow.tensorflow.load_model(model_uri)
+
+model = load_model()
+
+# Dictionnaire des races de chiens
+race_dict = {0: "BORZOI",
+             1: "MALINOIS",
+             2: "BORDER-COLLIE",
+             }
 
 # Interface Streamlit
 st.title("Quelle est la race du chien ?")
@@ -29,7 +36,8 @@ if st.button("Prendre une photo avec la webcam"):
         cv2.imwrite(img_path, frame)
         st.image(frame, channels="BGR")
         race = mt.prediction_race(model, img_path)
-        st.write(f"La race du chien est : {race}")
+        race_name = race_dict.get(race.item(), "Race inconnue")
+        st.write(f"La race du chien est : {race_name}")
     else:
         st.write("Impossible de prendre une photo. Vérifiez votre webcam.")
     cap.release()
@@ -41,4 +49,5 @@ if uploaded_file is not None:
     with open("temp.jpg", "wb") as f:
         f.write(uploaded_file.getbuffer())
     race = mt.prediction_race(model, "temp.jpg")
-    st.write(f"La race du chien est : {race}")
+    race_name = race_dict.get(race.item(), "Race inconnue")
+    st.write(f"La race du chien est : {race_name}")
